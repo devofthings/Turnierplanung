a8b06f963976b08c65e520a4ab5907a3940cc778
Chris Winter <chris-winter@live.de>
2021-02-23 12:53:21 +0100

    added a database

---
+++ Turnierplanung/Database/Datenbank-Daten.sql
@@ -0,0 +1,10 @@
+INSERT INTO participant (name, surname, age)
+VALUES ('Max', 'Mustermann', 30), ('Julian', 'Brandt', 25), ('Marco', 'Reus', 31), ('Stefi', 'Graf', 44), ('Boris', 'Becker', 50);
+
+INSERT INTO jobs (job)
+VALUES('Fußballspieler'), ('Tennisspieler'), ('Handballspieler'), ('Trainer'), ('Zeugwart'), ('Physiologe');
+
+INSERT INTO team (name) 
+VALUES ('Borussia Dortmund'), ('Test');
+
+INSERT INTO player (active, participant_id, team_id, job_id) VALUES (1, 1, 2, 5), (1, 2, 1, 1), (1, 3, 2, 1), (1, 4, 1, 1), (1, 5, 1, 2), (1, 0, 1, 2);
---
+++ Turnierplanung/Database/Datenbank-Shema.sql
@@ -0,0 +1,40 @@
+CREATE SCHEMA IF NOT EXISTS `tournament` DEFAULT CHARACTER SET utf8;
+USE `tournament` ;
+
+CREATE TABLE `tournament`.`team` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NULL DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tournament`.`jobs` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `job` VARCHAR(50) NOT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tournament`.`participant` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(50) NULL DEFAULT NULL,
+    `surname` VARCHAR(50) NULL DEFAULT NULL,
+    `age` INT(3) NULL DEFAULT NULL,
+    `job_id` INT(11) NULL DEFAULT NULL,
+    INDEX `fk_participant_job` (`job_id`),
+    CONSTRAINT `fk_participant_job` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`),
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `tournament`.`player` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `active` TINYINT(1) NULL,
+    PRIMARY KEY (`id`),
+    `participant_id` INT(11) NULL DEFAULT NULL,
+    INDEX `fk_player_participant` (`participant_id`),
+    CONSTRAINT `fk_player_participant` FOREIGN KEY (`participant_id`) REFERENCES `participant` (`id`),
+    `team_id` INT(11) NULL DEFAULT NULL,
+    INDEX `fk_player_team` (`team_id`),
+    CONSTRAINT `fk_player_team` FOREIGN KEY (`team_id`) REFERENCES `team` (`id`),
+    `job_id` INT(11) NULL DEFAULT NULL,
+    INDEX `fk_player_job` (`job_id`),
+    CONSTRAINT `fk_player_job` FOREIGN KEY (`job_id`) REFERENCES `jobs` (`id`)
+);
