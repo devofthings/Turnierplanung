051ba7b8f630fc00688c2b9e6dab025fb8156426
unknown <chris-winter@live.de>
2021-04-12 14:28:05 +0200

    fixed db bugs

--- Turnierverwaltung/Database/Datenbank-Shema.sql
+++ Turnierverwaltung/Database/Datenbank-Shema.sql
@@ -26,19 +26,22 @@ CREATE TABLE tournament.participants (
     lastname VARCHAR(50) NULL DEFAULT NULL,
     birthday DATE NULL DEFAULT NULL,
     job_id INT(11) NULL DEFAULT NULL,
-    health_status ENUM("Gesund", "Verletzt") DEFAULT 1,
+    health_status ENUM("Gesund", "Verletzt") DEFAULT "Gesund",
     PRIMARY KEY (id),
     INDEX participant_job (job_id),
-    CONSTRAINT participant_job FOREIGN KEY (jobs_id) REFERENCES jobs (id),
+    CONSTRAINT participant_job FOREIGN KEY (job_id) REFERENCES jobs (id)
+    /* TODO enable props
     INDEX participant_properties (id),
     CONSTRAINT participant_properties FOREIGN KEY (participant_id) REFERENCES participants_properties (participant_id)
+    */
 );
 
 CREATE TABLE tournament.participants_properties (
     id INT NOT NULL AUTO_INCREMENT,
-    participant_id INT(11) NOT NULL,
-    property_id INT(11) NOT NULL,
-    property_value VARCHAR(50) NULL DEFAULT NOT NULL
+    participant_id INT NOT NULL,
+    property_id INT NOT NULL,
+    property_value VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE tournament.participants_teams (
